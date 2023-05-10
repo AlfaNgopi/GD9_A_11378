179b8332fde935d08eaee258f8c795776cf9f53b
kenton <you@example.com>
2023-05-10 17:51:14 +0700

    seko kenton

--- src/dao/PustakaDAO.java
+++ src/dao/PustakaDAO.java
@@ -1,7 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// asdlasjdl
+// dasjkdah
+
 package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
